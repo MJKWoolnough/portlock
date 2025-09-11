019df997cf0c4c26a38bf075d41a59e48e7f2e88
Michael Woolnough <michael.woolnough@gmail.com>
2025-09-11 07:31:28 +0100

    re-formatted

--- portlock_test.go
+++ portlock_test.go
@@ -14,11 +14,14 @@ func HammerMutex(m Locker, loops int, cdone chan bool) {
 			if m.TryLock() {
 				m.Unlock()
 			}
+
 			continue
 		}
+
 		m.Lock()
 		m.Unlock()
 	}
+
 	cdone <- true
 }
 
@@ -26,24 +29,31 @@ func TestMutex(t *testing.T) {
 	if n := runtime.SetMutexProfileFraction(1); n != 0 {
 		t.Logf("got mutexrate %d expected 0", n)
 	}
+
 	defer runtime.SetMutexProfileFraction(0)
 
 	m := New(":9999")
 
 	m.Lock()
+
 	if m.TryLock() {
 		t.Fatalf("TryLock succeeded with mutex locked")
 	}
+
 	m.Unlock()
+
 	if !m.TryLock() {
 		t.Fatalf("TryLock failed with mutex unlocked")
 	}
+
 	m.Unlock()
 
 	c := make(chan bool)
+
 	for i := 0; i < 10; i++ {
 		go HammerMutex(m, 1000, c)
 	}
+
 	for i := 0; i < 10; i++ {
 		<-c
 	}
